cd5f95f5cd2bc41163db655e3c13aeabc27812a9
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-07-07 23:16:35 +0200

    runner with remote state + tfvars

--- runner/aws/isolated/mac-m1/destroy.sh
+++ runner/aws/isolated/mac-m1/destroy.sh
@@ -4,8 +4,8 @@ PROJECT="${1}"
 
 # set -exuo pipefail
 
-terraform init 
+terraform init -backend-config=environment/remote-state.tf
 
-terraform destroy -auto-approve \
-    -var key-name=${PROJECT} \
-    -var aws-region=${AWS_DEFAULT_REGION}
+terraform destroy -auto-approve -var-file=environment/variables.tfvars
+
+rm .tstenvs
--- runner/aws/isolated/mac-m1/main.tf
+++ runner/aws/isolated/mac-m1/main.tf
@@ -1,11 +1,27 @@
-variable key-name {}
-variable aws-region{}
+variable project-name       {}
+variable key-name           {}
+variable aws-region         {}
+variable availability-zones {}
+variable vpc-subnet-cidr    {}
+variable intra-subnet-cidr  {}
+variable public-subnet-cidr {}
   
 module key {
-  source     = "./../../../../infrastructure/aws/common/keys"
+  source                    = "./../../../../infrastructure/aws/common/keys"
 
-  key-name   = var.key-name
-  aws-region = var.aws-region
+  key-name                  = var.key-name
+  aws-region                = var.aws-region
+}
+
+module "network" {
+  source                    = "./../../../../infrastructure/aws/common/network"
+
+  aws-region                = var.aws-region
+  availability-zones        = var.availability-zones
+  project-name              = var.project-name
+  vpc-subnet-cidr           = var.vpc-subnet-cidr
+  intra-subnet-cidr         = var.intra-subnet-cidr
+  public-subnet-cidr        = var.public-subnet-cidr
 }
 
 # This module create isolated VMs as so only one public ip will be joined
@@ -17,4 +33,9 @@ output key-private {
 output key-public { 
   value = module.key.key-public  
   sensitive = true
+}
+
+# # Configuration on init time with -backend-config
+terraform {
+  backend "s3" {}
 }
