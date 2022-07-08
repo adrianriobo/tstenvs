c77f904725eb6fd91cef48511bc383c5491eb124
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-07-08 15:30:37 +0200

    Added support for isolated windows on AWS

--- infrastructure/aws/common/keys/main.tf
+++ infrastructure/aws/common/keys/main.tf
@@ -1,5 +1,5 @@
-variable aws-region   {}
-variable key-name     {}
+variable aws_region   {}
+variable key_name     {}
 variable algorithm    { default="RSA" }
 variable rsa_bits  { default="4096" }
 
@@ -9,14 +9,14 @@ resource tls_private_key this {
 }
 
 resource aws_key_pair this {
-  key_name   = var.key-name
+  key_name   = var.key_name
   public_key = tls_private_key.this.public_key_openssh
 
   provisioner local-exec { 
-    command = "echo '${tls_private_key.this.private_key_pem}' > .${var.key-name}"
+    command = "echo '${tls_private_key.this.private_key_pem}' > .${var.key_name}"
   }
 }
 
-output key-name     { value = aws_key_pair.this.key_name}
-output key-private  { value = tls_private_key.this.private_key_pem }
-output key-public   { value = tls_private_key.this.public_key_openssh }
+output key_name     { value = aws_key_pair.this.key_name}
+output key_private  { value = tls_private_key.this.private_key_pem }
+output key_public   { value = tls_private_key.this.public_key_openssh }
