67520674e4b8eb0ecf49063967cb5652ee6ac3d6
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-07-22 16:19:10 +0200

    aws windows target added local user + setup for sshd

--- infrastructure/aws/compute/guests/windows/data.tf
+++ infrastructure/aws/compute/guests/windows/data.tf
@@ -1,4 +1,12 @@
-variable win2019_hyperv_name   { default="Windows_Server-2019-English-Full-HyperV*" }
+variable win2019_hyperv_name      { default="Windows_Server-2019-English-Full-HyperV*" }
+variable hyperv_username          { default = "cloud-user"}
+variable key_public               {}
+
+resource random_password hyperv_password {
+  length           = 16
+  special          = true
+  override_special = "!#%&*()-_=+[]{}<>.?"
+}
 
 data aws_ami win2019_hyperv {
   most_recent = true
@@ -13,19 +21,60 @@ data aws_ami win2019_hyperv {
   owners = ["amazon"]
 }
 
-# Join domain
+# Setup ssh and basic config for user
+# Also inclusion on Hyper-v Administrator group should be handle by OpenshiftLocal msi 
+# but due to a bug it is not...we added to avoid 2 reboots 
+# Set-Service -Name sshd -StartupType ‘Automatic’
+# #Start-Service sshd
 locals {
    windows_setup = <<USERDATA
 <powershell>
+# Create local user
+$Password = ConvertTo-SecureString "${random_password.hyperv_password.result}" -AsPlainText -Force
+New-LocalUser ${var.hyperv_username} -Password $Password 
+# Run a process with new local user to create profile, so it will create home folder
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${var.hyperv_username}, $Password
+Start-Process cmd /c -WindowStyle Hidden -Wait -Credential $credential -ErrorAction SilentlyContinue
+# Add user to required groups
+Add-LocalGroupMember -Group "Administrators" -Member ${var.hyperv_username}
+# Check if this speed insall of crc...if msi installer checks if no reboot required
+Add-LocalGroupMember -Group "Hyper-V Administrators" -Member ${var.hyperv_username}
+# Set autologon to user to allow start sshd for the user 
+# Check requirements for domain user
+# https://docs.microsoft.com/en-us/troubleshoot/windows-server/user-profiles-and-logon/turn-on-automatic-logon
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
+Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "${var.hyperv_username}" -type String 
+Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "${random_password.hyperv_password.result}" -type String
+# Install sshd
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Set-Service -Name sshd -StartupType ‘Automatic’
+Set-Service -Name sshd -StartupType 'Manual'
+# This generate ssh certs + config file for us
 Start-Service sshd
+# Disable the service as need to start it as a user process on startup
+Stop-Service sshd
+# Add pub key for the user as authorized_key 
+New-Item -Path "C:\Users\${var.hyperv_username}\.ssh" -ItemType Directory -Force
+New-Item -Path C:\Users\${var.hyperv_username}\.ssh -Name "authorized_keys" -ItemType "file" -Value "${var.key_public}"
+# Set permissions valid permissions for hyper_user on authorized_keys + host_keys
+$acl = Get-Acl C:\Users\${var.hyperv_username}\.ssh\authorized_keys
+$acl.SetOwner([System.Security.Principal.NTAccount] "${var.hyperv_username}")
+$acl.SetAccessRuleProtection($True, $False)
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule([System.Security.Principal.NTAccount] "${var.hyperv_username}","FullControl","Allow")
+$acl.SetAccessRule($AccessRule)
+Set-Acl C:\Users\${var.hyperv_username}\.ssh\authorized_keys $acl
+Set-Acl -Path "C:\ProgramData\ssh\*key" $acl
+# Create bat script to start sshd as a user process on startup
+New-Item -Path "C:\Users\${var.hyperv_username}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Name start-openssh.bat -ItemType "file" -Value 'powershell -command "sshd -f C:\ProgramData\ssh\sshd_config"'
 </powershell>
 USERDATA
 }
 
 # To get subnets based on data, need to split the module creation
 # 1 network 2 compute...may we can do it for aggregate env
+# Add a check for values public or intra
+# variable vpc_id              {}
+# variable subnet_tier            { default="public" }
 # data aws_subnets this {
 #   filter {
 #     name   = "vpc-id"
@@ -39,3 +88,7 @@ USERDATA
 #     Tier = var.subnet_tier
 #   }
 # }
+# output subnet_id { value=data.aws_subnets.this.ids } 
+output hyperv_username { value=var.hyperv_username }
+output hyperv_password { value=random_password.hyperv_password.result }
+
--- infrastructure/aws/compute/guests/windows/main.tf
+++ infrastructure/aws/compute/guests/windows/main.tf
@@ -1,40 +1,71 @@
+variable project_name                   {}
+variable aws_region                     {}
 # Cost https://aws.amazon.com/ec2/pricing/on-demand/
 # Multi instance support https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/dedicated-hosts-overview.html
 # https://docs.amazonaws.cn/en_us/AWSEC2/latest/UserGuide/how-dedicated-hosts-work.html#dedicated-hosts-allocating
-variable dedicated-type         { default = "m5n" }
-variable instance-type          {
+variable dedicated_type                 { default = "m5n" }
+variable instance_type                  {
     type    = map(string)
     default = {
-        "m5n" = "m5n.2xlarge"
+        "m5n" = "m5n.metal"
     }
 }
-variable availability_zone      {}
-# variable vpc_id              {}
-variable subnet_id              {}   
-variable key_name               {}
+variable vpc_id                         {}
+variable vpc_default_security_group_id  {}
+variable availability_zone              {}
+variable subnet_id                      {}   
+variable key_name                       {}
+variable root_volume_size               { default = "100"}
 
-# Add a check for values public or intra
-# variable subnet_tier            { default="public" }
+# resource aws_ec2_host this {
+#   instance_family     = var.dedicated_type
+#   availability_zone   = var.availability_zone
+# }
 
-resource aws_ec2_host this {
-  instance_family     = var.dedicated-type
-  availability_zone = var.availability_zone
+module ssh_sg {
+  source = "terraform-aws-modules/security-group/aws//modules/ssh"
+
+  name        = "ssh"
+  description = "Access with ssh"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module rdp_sg {
+  source = "terraform-aws-modules/security-group/aws//modules/rdp"
+
+  name        = "rdp"
+  description = "Access with rdp"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource aws_instance this {
-  ami           = data.aws_ami.win2019_hyperv.id
-  instance_type = var.instance-type[var.dedicated-type]
-  host_id       = aws_ec2_host.this.id
-  subnet_id     = var.subnet_id
+  ami                 = data.aws_ami.win2019_hyperv.id
+  instance_type       = var.instance_type[var.dedicated_type]
+  # host_id             = aws_ec2_host.this.id
+  subnet_id           = var.subnet_id
   # Check AMI with ec2launch-v2
-#   user_data     = file("${path.module}/userdata.yaml")
-  user_data     = local.windows_setup
-  key_name      = var.key_name
+  # user_data     = file("${path.module}/userdata.yaml")
+  user_data           = local.windows_setup
+  key_name            = var.key_name
+  # Security groups
+  vpc_security_group_ids = [
+    module.ssh_sg.security_group_id, 
+    module.rdp_sg.security_group_id,
+    var.vpc_default_security_group_id
+  ]
+  
+  root_block_device {
+    volume_size     = var.root_volume_size
+  }
 
   tags = {
-    Name = "test"
+    Name = "win2019_hyperv-${var.project_name}"
   }
 }
 
-output win2019_hyperv_id { value=data.aws_ami.win2019_hyperv.id }
-# output subnet_id { value=data.aws_subnets.this.ids } 
+output win2019_hyperv_id  { value=data.aws_ami.win2019_hyperv.id }
+output instance_public_ip { value=aws_instance.this.public_ip }
